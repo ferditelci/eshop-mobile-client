d06fdb1ef05c05f2bb4a87c0e9ba47b6240146d8
Ertan Aydın <ertanaydinn@outlook.com>
2022-06-14 19:54:07 +0300

    Arama ve Urunler sayfaları eklendi.

---
+++ eShopOnContainers/eShopOnContainers.Core/Views/AraView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using eShopOnContainers.Core.ViewModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.Views
+{
+    public partial class AraView : ContentPage
+    {
+        private bool _animate;
+
+        public AraView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnDisappearing()
+        {
+            _animate = false;
+        }
+
+
+
+        private async Task AnimateItem(View uiElement, uint duration)
+        {
+            try
+            {
+                while (_animate)
+                {
+					await uiElement.ScaleTo(1.05, duration, Easing.SinInOut);
+					await Task.WhenAll(
+						uiElement.FadeTo(1, duration, Easing.SinInOut),
+						uiElement.LayoutTo(new Rectangle(new Point(0, 0), new Size(uiElement.Width, uiElement.Height))),
+						uiElement.FadeTo(.9, duration, Easing.SinInOut),
+						uiElement.ScaleTo(1.15, duration, Easing.SinInOut)
+					);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
